56e7be503f5418829c633ab433ce19be623f212b
Federico Zaiter <fzaiter@gmail.com>
2019-11-12 23:11:35 +0800

    Implemented PU Learning testing module. Also created a main args parser so each testing module modifieds or adds its concerning params.

---
+++ parse_args.py
@@ -0,0 +1,141 @@
+import os
+import argparse
+
+
+def init_main_args():
+    """Init command line args used for configuration."""
+
+    parser = argparse.ArgumentParser(
+        description="Runs binary classification with "
+                    + "PULearning to detect anomalous logs.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "--raw_logs",
+        metavar="raw_logs",
+        type=str,
+        nargs=1,
+        default=["./LogClass/data/rawlog.txt"],
+        help="input logs file path",
+    )
+    base_dir_default = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), "outputs"
+    )
+    parser.add_argument(
+        "--base_dir",
+        metavar="base_dir",
+        type=str,
+        nargs=1,
+        default=[base_dir_default],
+        help="base output directory for pipeline output files",
+    )
+    parser.add_argument(
+        "--logs",
+        metavar="logs",
+        type=str,
+        nargs=1,
+        default=[os.path.join(base_dir_default, "logs_without_paras.txt")],
+        help="input logs file path",
+    )
+    parser.add_argument(
+        "--logs_type",
+        metavar="logs_type",
+        type=str,
+        nargs=1,
+        default=["original"],
+        choices=["original", "bgl"],
+        help="Input type of logs.",
+    )
+    parser.add_argument(
+        "--kfold",
+        metavar="kfold",
+        type=int,
+        nargs=1,
+        default=[3],
+        help="kfold crossvalidation",
+    )
+    parser.add_argument(
+        "--healthy_label",
+        type=str,
+        nargs=1,
+        default=["unlabeled"],
+        help="the labels of unlabeled logs",
+    )
+    parser.add_argument(
+        "--features",
+        metavar="features",
+        type=str,
+        nargs='+',
+        default=["tfilf"],
+        choices=["tfidf", "tfilf", "length", "tf"],
+        help="Features to be extracted from the logs messages.",
+    )
+    parser.add_argument(
+        "--report",
+        metavar="report",
+        type=str,
+        nargs='+',
+        default=["confusion_matrix"],
+        choices=["confusion_matrix", "acc", "multi_acc", "top_k_svm"],
+        help="Reports to be generated from the model and its predictions.",
+    )
+    parser.add_argument(
+        "--binary_classifier",
+        metavar="binary_classifier",
+        type=str,
+        nargs=1,
+        default=["pu_learning"],
+        choices=["pu_learning", "regular"],
+        help="Binary classifier to be used as anomaly detector.",
+    )
+    parser.add_argument(
+        "--multi_classifier",
+        metavar="multi_classifier",
+        type=str,
+        nargs=1,
+        default=["svm"],
+        choices=["svm"],
+        help="Multi-clas classifier to classify anomalies.",
+    )
+    parser.add_argument(
+        "--train",
+        action="store_true",
+        default=False,
+        help="If set, logclass will train on the given data. Otherwise"
+             + "it will run inference on it.",
+    )
+    parser.add_argument(
+        "--preprocess",
+        action="store_true",
+        default=False,
+        help="If set, the raw logs parameters will be preprocessed and a "
+             + "new file created with the preprocessed logs.",
+    )
+    parser.add_argument(
+        "--force",
+        action="store_true",
+        default=False,
+        help="force training overwriting previous output.",
+    )
+
+    return parser
+
+
+def parse_main_args(args):
+    """Parse provided args for runtime configuration."""
+    params = {
+        "logs": args.logs[0],
+        "raw_logs": args.raw_logs[0],
+        "kfold": args.kfold[0],
+        "healthy_label": args.healthy_label[0],
+        "report": args.report,
+        "train": args.train,
+        "preprocess": args.preprocess,
+        "force": args.force,
+        "base_dir": args.base_dir[0],
+        "logs_type": args.logs_type[0],
+        "features": args.features,
+        "binary_classifier": args.binary_classifier[0],
+        "multi_classifier": args.multi_classifier[0],
+    }
+    return params
---
+++ test_pu.py
@@ -0,0 +1,169 @@
+from sklearn.model_selection import StratifiedKFold
+from .utils import (
+    file_handling,
+    extract_features,
+    TestingParameters,
+)
+from .preprocess import registry as preprocess_registry
+from .preprocess.utils import load_logs
+from .feature_engineering.utils import binary_train_gtruth
+from uuid import uuid4
+from .models import binary_registry as binary_classifier_registry
+from .reporting import bb_registry as black_box_report_registry
+from .parse_args import init_main_args, parse_main_args
+import numpy as np
+
+
+def init_flags():
+    """Init command line flags used for configuration."""
+
+    parser = init_main_args()
+    parser.add_argument(
+        "--ratio",
+        metavar="ratio",
+        type=int,
+        nargs=1,
+        default=[8],
+        help="ratio",
+    )
+    parser.add_argument(
+        "--top_percentage",
+        metavar="top_percentage",
+        type=int,
+        nargs=1,
+        default=[11],
+        help="top_percentage",
+    )
+    parser.add_argument(
+        "--step",
+        metavar="step",
+        type=int,
+        nargs=1,
+        default=[2],
+        help="step",
+    )
+    return parser.parse_args()
+
+
+def parse_args(args):
+    """Parse provided args for runtime configuration."""
+    params = parse_main_args(args)
+    additional_params = {
+                            "ratio": args.ratio[0],
+                            "top_percentage": args.top_percentage[0],
+                            "step": args.step[0],
+                            "train": True,
+                        }
+    params.update(additional_params)
+    return params
+
+
+def print_params(params):
+    print("{:-^80}".format("params"))
+    print("Beginning binary classification "
+          + "using the following configuration:\n")
+    for param, value in params.items():
+        print("\t{:>13}: {}".format(param, value))
+    print()
+    print("-" * 80)
+
+
+def force_ratio(params, x_data, y_data):
+    """Force a ratio between anomalous and healthy logs"""
+    ratio = params['ratio']
+    if ratio > 0:
+        anomalous = np.where(y_data == 1.0)[0]
+        healthy = np.where(y_data == -1.0)[0]
+        if len(anomalous) * ratio <= len(healthy):
+            keep_anomalous = len(anomalous)
+            keep_healthy = keep_anomalous * ratio
+        else:
+            keep_anomalous = len(healthy) // ratio
+            keep_healthy = len(healthy)
+        np.random.seed(10)
+        permut = np.random.permutation(len(healthy))
+        keep = permut[:keep_healthy]
+        healthy = healthy[keep]
+        permut = np.random.permutation(len(anomalous))
+        keep = permut[:keep_anomalous]
+        anomalous = anomalous[keep]
+        result = sorted(np.concatenate((anomalous, healthy)))
+        y_data = y_data[result]
+        x_data = x_data[result]
+        return x_data, y_data
+
+
+def run_test(params, x_data, y_data):
+    # Binary training features
+    y_data = binary_train_gtruth(y_data)
+    x_data, y_data = force_ratio(params, x_data, y_data)
+    print("total logs", len(y_data))
+    print(len(np.where(y_data == -1.0)[0]), " are unlabeled")
+    print(len(np.where(y_data == 1.0)[0]), " are anomalous")
+    # KFold Cross Validation
+    kfold = StratifiedKFold(n_splits=params['kfold']).split(x_data, y_data)
+    for train_index, test_index in kfold:
+        params['experiment_id'] = str(uuid4().time_low)
+        print(f"\nExperiment ID: {params['experiment_id']}")
+        x_train, x_test = x_data[train_index], x_data[test_index]
+        y_train, y_test = y_data[train_index], y_data[test_index]
+        x_train, _ = extract_features(x_train, params)
+        with TestingParameters(params):
+            x_test, _ = extract_features(x_test, params)
+        np.random.seed(5)
+        permut = np.random.permutation(len(y_train))
+        x_train = x_train[permut]
+        y_train = y_train[permut]
+        top_percentage = params['top_percentage']
+        step = params['step']
+        # Relabeling anomalous logs to unlabeled to test PU Learning Robustness
+        for i in range(0, top_percentage, step):
+            y_train_pu = np.copy(y_train)
+            if i > 0:
+                n_unlabeled = len(np.where(y_train_pu == -1.0)[0])
+                sacrifice_size = (i*n_unlabeled)//(100 - i)
+                print(i, n_unlabeled, sacrifice_size)
+                pos = np.where(y_train == 1.0)[0]
+                np.random.shuffle(pos)
+                sacrifice = pos[: sacrifice_size]
+                y_train_pu[sacrifice] = -1.0
+
+            print(f"{i}% of anomalous log in unlabeled logs:")
+            print(len(np.where(y_train_pu == -1.0)[0]), " are unlabeled")
+            print(len(np.where(y_train_pu == 1.0)[0]), " are anomalous")
+            # Binary PULearning with RF
+            pu_getter =\
+                binary_classifier_registry.get_binary_model("pu_learning")
+            binary_clf = pu_getter(params)
+            binary_clf.fit(x_train, y_train_pu)
+            y_pred_pu = binary_clf.predict(x_test)
+            get_accuracy = black_box_report_registry.get_bb_report('acc')
+            pu_acc = get_accuracy(y_test, y_pred_pu)
+            # Comparing given Binary Classifier with PU Learning
+            comparison_clf_getter =\
+                binary_classifier_registry.get_binary_model(
+                    params['binary_classifier'])
+            binary_clf = comparison_clf_getter(params)
+            binary_clf.fit(x_train, y_train_pu)
+            y_pred = binary_clf.predict(x_test)
+            b_clf_acc = get_accuracy(y_test, y_pred)
+            print(f"PU Acc: {pu_acc}\n{params['binary_classifier']} Acc: {b_clf_acc}")
+
+
+def main():
+    # Init params
+    params = parse_args(init_flags())
+    print_params(params)
+    file_handling(params)
+    # Filter params from raw logs
+    if params['preprocess']:
+        preprocess = preprocess_registry.get_preprocessor(params['logs_type'])
+        preprocess(params)
+    # Load filtered params from file
+    print('Loading logs')
+    x_data, y_data, _ = load_logs(params)
+    run_test(params, x_data, y_data)
+
+
+if __name__ == "__main__":
+    main()
