62c4cf95c301959302d83725443ec50cdd3f67cc
Federico Zaiter <fzaiter@gmail.com>
2019-12-01 12:31:57 +0800

    Added docstrings for the BaseModel class and its functions to aid the readme.

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,31 @@ from ..decorators import print_step
 
 
 class BaseModel(ABC):
+    """ Abstract class used to wrap models and add further functionality.
+
+        Attributes
+        ----------
+        model : model that implements fit and predict functions as sklearn
+        ML models do.
+        params : dict of experiment parameters.
+        name : str of the original model class name.
+        train_time : time it took to run fit in seconds.
+        run_time : time it took to run predict in seconds.
+
+        Methods
+        -------
+        save(self, **kwargs)
+            Abstract method for the subclass to implement how the model is
+            saved. Should use the experiment id as reference.
+        load(self, **kwargs)
+            Abstract method for the subclass to implement how it's meant to be
+            loaded. Should correspond to how the save method saves the model.
+        predict(self, X, **kwargs)
+            Wraps original model predict and times its running time.
+        fit(self, X, Y, **kwargs)
+            Wraps original model fit, times fit running time and saves the model.
+
+    """
     def __init__(self, model, params):
         self.model = model
         self.params = params
@@ -13,14 +38,25 @@ class BaseModel(ABC):
 
     @abstractmethod
     def save(self, **kwargs):
+        """
+            Abstract method for the subclass to implement how the model is
+            saved. Should use the experiment id as reference.
+        """
         pass
 
     @abstractmethod
     def load(self, **kwargs):
+        """
+            Abstract method for the subclass to implement how it's meant to be
+            loaded. Should correspond to how the save method saves the model.
+        """
         pass
 
     @print_step
     def predict(self, X, **kwargs):
+        """
+            Wraps original model predict and times its running time.
+        """
         t0 = time()
         pred = self.model.predict(X, **kwargs)
         t1 = time()
@@ -31,6 +67,9 @@ class BaseModel(ABC):
 
     @print_step
     def fit(self, X, Y, **kwargs):
+        """
+            Wraps original model fit, times fit running time and saves the model.
+        """
         t0 = time()
         self.model.fit(X, Y, **kwargs)
         t1 = time()
