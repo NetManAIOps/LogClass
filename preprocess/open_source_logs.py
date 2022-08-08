5c1bc64eeadf7bc762d61e14a755ea53219b06da
Federico Zaiter <fzaiter@gmail.com>
2022-08-08 11:12:34 -0300

    Fix open source logs preprocess dataset function

--- preprocess/open_source_logs.py
+++ preprocess/open_source_logs.py
@@ -1,29 +1,31 @@
-from .registry import register
-from .utils import remove_parameters
-from tqdm import tqdm
 import os
 from multiprocessing import Pool
+
+from tqdm import tqdm
+
+from .registry import register
+from .utils import remove_parameters
 
 
 def process_line(line):
     label = line[0].strip()
-    msg = ' '.join(line[1].strip().split()[1:])
+    msg = " ".join(line[1].strip().split()[1:])
     msg = remove_parameters(msg)
     if msg:
-        msg = ' '.join((label, msg))
-        msg = ''.join((msg, '\n'))
+        msg = " ".join((label, msg))
+        msg = "".join((msg, "\n"))
         return msg
-    return ''
+    return ""
 
 
 def process_open_source(input_source, output):
-    with open(output, "w", encoding='latin-1') as f:
+    with open(output, "w", encoding="latin-1") as f:
         gtruth = os.path.join(input_source, "groundtruth.seq")
         rawlog = os.path.join(input_source, "rawlog.log")
-        with open(gtruth, 'r', encoding='latin-1') as IN:
+        with open(gtruth, "r", encoding="latin-1") as IN:
             line_count = sum(1 for line in IN)
-        with open(gtruth, 'r', encoding='latin-1') as in_gtruth:
-            with open(rawlog, 'r', encoding='latin-1') as in_log:
+        with open(gtruth, "r", encoding="latin-1") as in_gtruth:
+            with open(rawlog, "r", encoding="latin-1") as in_log:
                 IN = zip(in_gtruth, in_log)
                 with Pool() as pool:
                     results = pool.imap(process_line, IN, chunksize=10000)
@@ -31,24 +33,22 @@ def process_open_source(input_source, output):
 
 
 open_source_datasets = [
-    'open_Apache',
-    'open_bgl',
-    'open_hadoop',
-    'open_hdfs',
-    'open_hpc',
-    'open_proxifier',
-    'open_zookeeper',
+    "open_Apache",
+    "open_bgl",
+    "open_hadoop",
+    "open_hdfs",
+    "open_hpc",
+    "open_proxifier",
+    "open_zookeeper",
 ]
 for dataset in open_source_datasets:
+
     @register(dataset)
     def preprocess_dataset(params):
         """
         Runs open source logs preprocessing executor.
         """
-        input_source = os.path.join(
-            params['raw_logs'],
-            dataset.split('_')[-1]
-        )
-        output = params['logs']
-        params['healthy_label'] = 'NA'
+        input_source = params["raw_logs"]
+        output = params["logs"]
+        params["healthy_label"] = "NA"
         process_open_source(input_source, output)
